968a47a5e5571f9a62421dbf9ac1bfdcd510fc1c
jykim111 <jykim1@email.unc.edu>
2022-04-19 05:28:21 -0400

    Updated routes w/ better name semantics

--- src/routes/coin_routes.js
+++ src/routes/coin_routes.js
@@ -4,37 +4,37 @@
 const express = require('express');
 
 // Define router object that is an instance used extensively for express.
-const router = express.Router();
+const coin_router = express.Router();
 
 // Import middleware to use for the routes.
 const flip_middleware = require('../middleware/flip_middleware');
 
 // Define check endpoint
-router.get('/', flip_middleware.status);
+coin_router.get('/', flip_middleware.status);
 
 // Define router for multiple coin flips.
-router.post('/flip/coins/', flip_middleware.multiple_coins);
+coin_router.post('/flip/coins/', flip_middleware.multiple_coins);
 
 
 // Define router for coinFlip.
-router.get('/flip/', flip_middleware.coin_flip);
+coin_router.get('/flip/', flip_middleware.coin_flip);
 
 // Define response for coinFlips with given number.
-router.get('/flips/:number', flip_middleware.number_coin_flip);
+coin_router.get('/flips/:number', flip_middleware.number_coin_flip);
 
 
 // Define response for call.
-router.post('/flip/call/', flip_middleware.flip_call);
+coin_router.post('/flip/call/', flip_middleware.flip_call);
 
 // Define response for heads call.
-router.get('/flip/call/heads', flip_middleware.head_call);
+coin_router.get('/flip/call/heads', flip_middleware.head_call);
 
 
 // Define response for tails call.
-router.get('/flip/call/tails', flip_middleware.tail_call);
+coin_router.get('/flip/call/tails', flip_middleware.tail_call);
 
 // Define response for Guessing heads or tails.
-router.get('/flip/call/:guess(heads|tails)/', flip_middleware.guess_flip);
+coin_router.get('/flip/call/:guess(heads|tails)/', flip_middleware.guess_flip);
 
 
 module.exports = coin_router;
--- src/routes/debug_route.js
+++ src/routes/debug_route.js
@@ -7,13 +7,13 @@
 const express = require('express');
 
 // Define router object that is an instance used extensively for express.
-const router = express.Router();
+const debug_router = express.Router();
 
 // Import database
 const database = require('../services/database');
 
 // endpoint for /app/log/access that returns all records in "accesslog" table in "log.db"
-router.get('/log/access', (req, res) => {
+debug_router.get('/log/access', (req, res) => {
     try {
         const statement = database.prepare('SELECT * FROM accesslog').all();
         res.status(200).json(statement);
@@ -23,7 +23,7 @@ router.get('/log/access', (req, res) => {
 })
 
 // endpoint for /app/error if there is an error.
-router.get('/error', (req, res) => {
+debug_router.get('/error', (req, res) => {
     throw new Error('Error test successful.');
 })
 

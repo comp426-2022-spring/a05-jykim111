3a067d31e95bb067a88401de88a5b9193514ae3c
jykim111 <jykim1@email.unc.edu>
2022-04-20 01:09:24 -0400

    Worked on Single Flip

--- public/main.js
+++ public/main.js
@@ -1,6 +1,63 @@
 // Focus div based on nav button click
 
+const home = document.getElementById("homenav");
+home.addEventListener("click", activeHome);
+function activeHome() {
+    document.getElementById("home").className = "active";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "hidden";
+}
+
+const single = document.getElementById("singlenav");
+single.addEventListener("click", activeSingle);
+function activeSingle() {
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "active";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "hidden";
+}
+
+const multiple = document.getElementById("multinav");
+multiple.addEventListener("click", activeMultiple);
+function activeMultiple() {
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "active";
+    document.getElementById("guess").className = "hidden";
+}
+
+const guess = document.getElementById("guessnav");
+guess.addEventListener("click", activeGuess);
+function activeGuess() {
+    document.getElementById("home").className = "hidden";
+    document.getElementById("single").className = "hidden";
+    document.getElementById("multi").className = "hidden";
+    document.getElementById("guess").className = "active";
+}
+
 // Flip one coin and show coin image to match result when button clicked
+
+// Button coin flip element.
+const coin = document.getElementById("coin");
+// Add event listener for coin button.
+coin.addEventListener("click", singleCoinFlip);
+
+function singleCoinFlip() {
+    fetch('http://localhost:5000/app/flip')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            // It is result.flip b/c "json()" returns key/value pair, and the key/value pair in
+            // that endpoint is "flip": "heads", so the value in flip will be displayed (head/tails)
+            document.getElementById("singleFlipResult").innerHTML = result.flip;
+            document.getElementById("quarter").setAttribute("src", "./assets/img/" + result.flip + ".png");
+            //coin.disabled = true;
+
+        })
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series

a708a24ec2c246d643725c27c32afd80fdf0be11
jykim111 <jykim1@email.unc.edu>
2022-04-22 01:20:17 -0400

    Added multiple coin flips in main.js

--- public/main.js
+++ public/main.js
@@ -62,4 +62,87 @@ function singleCoinFlip() {
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
+// Our flip many coins form
+const coins = document.getElementById("coins");
+
+// Add event listener for coins form
+coins.addEventListener("submit", flipCoins);
+
+// Create the submit handler. The code above goes into this function which is an event.
+async function flipCoins(event) {
+    // Prevents the default behavior of the browser submitting the form
+    // so that we can handle things instead.
+    event.preventDefault();
+
+    const endpoint = "app/flip/coins/";
+
+    // document.baseURI gives something that you can then use to build on top of.
+    // then add endpoint to url to redirect.
+    const url = document.baseURI + endpoint;
+
+    // This gets the element which the event handler was attached to.
+    const formEvent = event.currentTarget;
+
+    try {
+        // This takes all the fields in the form and makes their values
+        // available throught a 'FormData' instance.
+        const formData = new FormData(formEvent);
+
+        const flips = await sendFlips({ url, formData });
+
+        console.log(flips);
+        document.getElementById("heads").innerHTML = "Heads: " + flips.summary.heads;
+        document.getElementById("tails").innerHTML = "Tails: " + flips.summary.tails;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Create a data sender
+async function sendFlips({ url, formData }) {
+    /** 
+     * We can't pass the 'FormData' instance directly to 'fetch'
+     * as that will cause it to automatically format the request
+     * body as "multipart" and set the 'Content-Type' request header
+     * to 'multipart/form-data'. We want to send the request body
+     * as JSON, so we're converting it to a plain object and then
+     * into a JSON string.
+     */
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJson = JSON.stringify(plainFormData);
+    console.log(formDataJson);
+
+
+    const options = {
+        /**
+         * The default method for a request with fetch is GET,
+         * so we must tell it to use POST HTTP method.
+         */
+        method: "POST",
+        /**
+         * These headers will be added to the request and tell
+         * the API that the request body is JSON and that we can
+         * accept JSON responses.
+         */
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        /**
+         * The body of our POST request is the JSON string that
+         * we created above.
+         */
+        body: formDataJson
+    };
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        const error = await response.text();
+        throw new Error(error);
+    }
+
+    return response.json();
+}
+
 // Guess a flip by clicking either heads or tails button
